9f7cd1ab587a0e34db42c63a901af4425e46313f
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2023-11-10 19:21:36 +0100

    Resolution to BFN1

--- BFN1/main.py
+++ BFN1/main.py
@@ -1,18 +1,37 @@
 import sys
 
-def isPalidrome(numbers, lenght):
+def isPalindrome(numbers):
+    lenght = len(numbers)-1
     for j in (0, lenght):
-        if int(numbers[j]) == int(numbers[(lenght-j)]):
-            return (numbers, 0)
-        else:
-            print ("chuj")
+        if int(numbers[j]) != int(numbers[(lenght-j)]):
+            return False
+    return True
+    pass
+
+def reverse(numbers):
+    lenght = len(numbers)-1
+    numbersReversed = ""
+    for k in range (0, lenght+1):
+        numbersReversed += (numbers[lenght-k])
+    return int(numbersReversed)
+
+
 
 n = input()
 for i in range(0, int(n)):
     numbers = input()
-    lenght = len(numbers)-1
-    isPalidrome(numbers, lenght)
-    
+    counter = 0
+
+    if isPalindrome(numbers):
+        print (numbers, 0)
+    else:
+        while isPalindrome(numbers) == False:
+            numbers = int(numbers) + reverse(numbers)
+            numbers = str(numbers)
+            counter += 1
+        print (numbers, counter)
+
+
 
 
 
