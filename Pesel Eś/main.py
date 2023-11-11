1a9a3dd71b819da77e02d0158fcfb25edf84ac85
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2023-11-11 09:42:19 +0100

    Resolution to PESEL

--- Pesel Eś/main.py
+++ Pesel Eś/main.py
@@ -8,19 +8,19 @@ if len(pesel) == 11:
     if int(day) < 32:
         if int(month) > 0 and int(month) < 13:
             year = "19" + pesel[0] + pesel[1]
-            print (day + "/" + month + "/" + year)
+            print ("Twoja data urodzin: " + day + "/" + month + "/" + year)
         elif int(month) > 20 and int(month) < 33:
             year = "20" + pesel[0] + pesel[1]
             month = str((int(month) - 20))
             if len(month) == 1:
                 month = "0" + month
-            print (day + "/" + month + "/" + year)
+            print ("Twoja data urodzin: " + day + "/" + month + "/" + year)
         elif int(month) > 80 and int(month) < 93:
             year = "18" + pesel[0] + pesel[1]
             month = str((int(month) - 80))
             if len(month) == 1:
                 month = "0" + month
-            print (day + "/" + month + "/" + year)
+            print ("Twoja data urodzin: " + day + "/" + month + "/" + year)
         else:
             print ("Nieprawidłowy format numeru PESEL.")
     else:
